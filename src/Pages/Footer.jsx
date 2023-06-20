6503863422689e2c50e1c6157a0eb6c30b1adb59
dalepatrick <dalepatrick005@gmail.com>
2023-06-20 18:54:08 +0800

    change class to className

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,23 +3,23 @@
 function Footer() {
     return (
         <>
-            <footer class="container-fluid mx-auto">
-        <div class="row">
-            <div class="col-md-4">
-                <h1 class="ms-5">Curve</h1>
-                <p class="mt-2 ms-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere dolorum placeat
+            <footer className="container-fluid mx-auto">
+        <div className="row">
+            <div className="col-md-4">
+                <h1 className="ms-5">Curve</h1>
+                <p className="mt-2 ms-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere dolorum placeat
                     doloribus obcaecati ex earum numquam incidunt enim, neque, delectus perferendis! Labore eum,
                     inventore nostrum ipsam corrupti repudiandae dignissimos minus!</p>
-                <div class="icon ms-4 p-2">
-                    <i class="fa-brands fa-twitter"></i>
-                    <i class="fa-brands fa-facebook"></i>
-                    <i class="fa-brands fa-discord"></i>
-                    <i class="fa-brands fa-instagram"></i>
+                <div className="icon ms-4 p-2">
+                    <i className="fa-brands fa-twitter"></i>
+                    <i className="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-discord"></i>
+                    <i className="fa-brands fa-instagram"></i>
                 </div>    
             </div>
 
-            <div class="col-md-2">
-                <div class="support ms-2">
+            <div className="col-md-2">
+                <div className="support ms-2">
                     <h3>Support</h3>
                     <p>
                         <a href=""><small>How to Hire</small></a>
@@ -35,8 +35,8 @@ function Footer() {
                     </p>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="ms-4">
+            <div className="col-md-3">
+                <div className="ms-4">
                     <h3>Contact Us</h3>
                     <strong>Location</strong>
                     <p>Manila, Philippines</p>
@@ -48,17 +48,17 @@ function Footer() {
                     <p>support@curve.com</p>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div className="col-md-3">
                 <div>
                     <h3>Newsletter Signup</h3>
-                    <input type="text" placeholder="Your Email Address" class="mt-3"></input>
-                    <button class="btn btn-primary fw-bold">Subscribe Now</button>
-                    <p class="mt-3"><small>Get the latest update annd offer for business service yearly</small></p>
+                    <input type="text" placeholder="Your Email Address" className="mt-3"></input>
+                    <button className="btn btn-primary fw-bold">Subscribe Now</button>
+                    <p className="mt-3"><small>Get the latest update annd offer for business service yearly</small></p>
                 </div>
             </div>
         </div>
 
-        <div class="text-center mt-2">
+        <div className="text-center mt-2">
             <p>© Copyright 2023 Curve. All Right Reserve</p>
         </div>
     </footer>
