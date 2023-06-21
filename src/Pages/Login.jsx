10eeb4f23131698ea7b077e34c327ca2e9b62465
xEliezer <layugeliezerbongco16@gmail.com>
2023-06-21 11:49:37 +0800

    fix an error

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,15 +5,6 @@ function Login() {
     return (
 
         <>
-<<<<<<< HEAD
-            <div className="container mt-5" id="Login">
-                <form className="loginform border shadow rounded p-5">
-                    <h1 className="form-title text-center text-white">Log-In</h1>
-                    <div className="mb-3 form-input">
-                        <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-
-=======
             <form className="container">
                 <div className="rounded">
                     <div>
@@ -26,7 +17,6 @@ function Login() {
                             <label htmlFor ="inputPassword2" className="visually-hidden">Password</label>
                             <input type="password" className="form-control" id="inputPassword2" placeholder="Password"/>
                         </div>
->>>>>>> main
                     </div>
                     <div className="mb-3 form-input">
                         <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
@@ -34,12 +24,12 @@ function Login() {
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label text-white" for="exampleCheck1">Keep me Log-In</label>
+                        <label className="form-check-label text-white" htmlFor="exampleCheck1">Keep me Log-In</label>
                     </div>
                     <button type="submit" className="btn btn-primary ">Submit</button>
                     <div id="emailHelp" className="form-text text-white">forgot password</div>
-                </form>
-            </div>
+                </div>
+            </form>
         </>
     )
 }
