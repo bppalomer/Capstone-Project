07ea31bd76abcdfd5664d4a031db172ab80f014d
bppalomer <bppalomer@gmail.com>
2023-06-29 23:57:24 +0800

    Update the about us page first part

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,11 +1,98 @@
+import { useState } from "react";
+
+const About = () => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const buttonData = {
+    developer: <div className="d-flex">
+<div className="card about_card justify-content-center d-flex align-items-center m-4">
+  <img className="card-img-top about_img_card m-2" src="/bryan.png" alt="Card image cap" />
+  <div className="card-body text-center">
+    <h5 className="card-title fw-bold">Bryan Paul Palomer</h5>
+    <p className="card-text fw-bold">Back-End Developer</p>
+    <a class="text-dark" href=""><i class="fa-brands fa-github m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-linkedin m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-facebook m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-instagram m-3"></i></a>
+  </div>
+</div>
+<div className="card about_card justify-content-center d-flex align-items-center m-4">
+  <img className="card-img-top about_img_card m-2" src="/bryan.png" alt="Card image cap" />
+  <div className="card-body text-center">
+    <h5 className="card-title fw-bold">Bryan Paul Palomer</h5>
+    <p className="card-text fw-bold">Back-End Developer</p>
+    <a class="text-dark" href=""><i class="fa-brands fa-github m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-linkedin m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-facebook m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-instagram m-3"></i></a>
+  </div>
+</div>
+<div className="card about_card justify-content-center d-flex align-items-center m-4">
+  <img className="card-img-top about_img_card m-2" src="/bryan.png" alt="Card image cap" />
+  <div className="card-body text-center">
+    <h5 className="card-title fw-bold">Bryan Paul Palomer</h5>
+    <p className="card-text fw-bold">Back-End Developer</p>
+    <a class="text-dark" href=""><i class="fa-brands fa-github m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-linkedin m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-facebook m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-instagram m-3"></i></a>
+  </div>
+</div>
+<div className="card about_card justify-content-center d-flex align-items-center m-4">
+  <img className="card-img-top about_img_card m-2" src="/bryan.png" alt="Card image cap" />
+  <div className="card-body text-center">
+    <h5 className="card-title fw-bold">Bryan Paul Palomer</h5>
+    <p className="card-text fw-bold">Back-End Developer</p>
+    <a class="text-dark" href=""><i class="fa-brands fa-github m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-linkedin m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-facebook m-3"></i></a>
+    <a class="text-dark" href=""><i class="fa-brands fa-instagram m-3"></i></a>
+  </div>
+</div>
 
 
-function About () {
-    return (
-        <>
-            <h1>This is an about page</h1>
-        </>
-    )
-}
+
+    </div>,
+    faqs: <div>This is button 2</div>,
+    privacy_policy: <div>This is button 3</div>,
+  };
+
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName);
+  };
+
+  return (
+    <>
+      <div className="container d-flex justify-content-center m-5 mx-auto">
+        <div>
+          <button
+            onClick={() => handleButtonClick("developer")}
+            className="rounded m-2 about_btn"
+          >
+            Developers
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick("faqs")} className="rounded m-2 about_btn">
+            FAQ's
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => handleButtonClick("privacy_policy")}
+            className="rounded m-2 about_btn"
+          >
+            Privacy Policy
+          </button>
+        </div>
+      </div>
+      <div className="justify-content-center mx-auto d-flex">
+        {selectedButton && (
+          <div className="text-light">{buttonData[selectedButton]}</div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default About;
