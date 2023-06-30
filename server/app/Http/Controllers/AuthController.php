937b71a9e77b4ea4caf6829a2ef50e274234f754
bppalomer <bppalomer@gmail.com>
2023-06-30 16:48:09 +0800

    Added aditional commands in server backend to connect it to front end.

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function userregister() {
+
+    }
+
+    public function recruiterregister() {
+        
+    }
+
+    public function login() {
+        
+    }
+}
