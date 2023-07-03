61430d1e0b19549dca5111f6b75f72839b0e708c
dalepatrick <dalepatrick005@gmail.com>
2023-07-03 09:43:58 +0800

    add FAQs pages

---
+++ client/src/Pages/FAQs.jsx
@@ -0,0 +1,14 @@
+function FAQs() {
+    return (
+        <>
+            <div class="container mt-4">
+                <div class="border rounded">
+                    <p class="ms-3 mt-3">Q: What is a Privacy Policy?</p>
+                    <p class="ms-3 mt-2">A: A Privacy Policy is a legal document that explains how an organization collects, uses, discloses, and protects the personal information of individuals. It outlines the rights and choices of users regarding their personal data and informs them about the security measures in place to safeguard their information.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default FAQs
